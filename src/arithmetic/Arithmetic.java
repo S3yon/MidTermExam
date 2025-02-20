e57ad1ea3f42e2be5d054de7182a82fdf88fa209
S3yon <sriskans@sheridancollege.ca>
2025-02-20 18:10:31 -0500

    Finished changes

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -15,22 +15,20 @@ import static java.time.Clock.system;
  * @author sivagamasrinivasan
  * 
  */
-public class Arithmetic 
-{
+public class Arithmetic {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-       
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
-    
+        System.out.println("Enter first number: ");
+        double n = in.nextDouble();
+        System.out.println("Enter second number: ");
+        double m = in.nextDouble();
+
+        System.out.println("Enter operation (PLUS, MINUS, TIMES, DIVIDE): ");
+        Operation op = Operation.valueOf(in.next().toUpperCase());
+
+        ArithmeticBase r = new ArithmeticBase();
+        double result = r.calculate(n, m, op);
+        System.out.println("Result: " + result);
     }
 }
-
--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -13,27 +13,26 @@ import java.util.Scanner;
  * @author sivagamasrinivasan
  * 
  */
-public class ArithmeticBase 
-{
- public double x,y;
-    double calculate(double x, double y) 
-        {
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter arithmetic operation to Perform: ");
-        String s= sc.next();
-        switch (s.toUpperCase()) 
-        {
-            case "PLUS":
+enum Operation {
+    PLUS, MINUS, TIMES, DIVIDE;
+}
+
+public class ArithmeticBase {
+    public double calculate(double x, double y, Operation op) {
+        switch (op) {
+            case PLUS:
                 return x + y;
-            case "MINUS":
+            case MINUS:
                 return x - y;
-            case "TIMES":
+            case TIMES:
                 return x * y;
-            case "DIVIDE":
+            case DIVIDE:
+                if (y == 0) {
+                    throw new ArithmeticException("Cannot divide by zero");
+                }
                 return x / y;
             default:
-                throw new AssertionError("Unknown operations " + this);
+                throw new IllegalArgumentException("Invalid operation");
         }
     }
-   
 }
